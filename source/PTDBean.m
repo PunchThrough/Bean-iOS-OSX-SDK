bc792a6f780547fb8c58ec8248c14277ddf4fefb
Kevin Marx <kevindmarx@gmail.com>
2014-12-04 14:51:31 -0600

    add method to send a message to release the serial gate

--- source/PTDBean.m
+++ source/PTDBean.m
@@ -128,6 +128,10 @@ typedef enum { //These occur in sequence
 }
 
 #pragma mark SDK
+- (void)releaseSerialGate {
+  [appMessageLayer sendMessageWithID:MSG_ID_BT_END_GATE andPayload:nil];
+}
+
 - (BOOL)setPairingPin:(NSUInteger*)pinCode{
     if(![self connected]) {
         return FALSE;
