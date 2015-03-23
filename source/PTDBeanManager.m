c72efd38902224f6e4774661baed8f80efce18c9
Ray Kampmeier <raykampmeier@gmail.com>
2015-03-23 11:08:39 -0700

    - Additional condition

--- source/PTDBeanManager.m
+++ source/PTDBeanManager.m
@@ -154,7 +154,8 @@
 -(void)disconnectFromAllBeans:(NSError **)error {
     for (NSString* key in beanRecords) {
         PTDBean* bean = [beanRecords objectForKey:key];
-        if (bean.state == BeanState_ConnectedAndValidated) {
+        if (bean.state == BeanState_ConnectedAndValidated
+            || bean.state == BeanState_AttemptingValidation) {
             [self disconnectBean:bean error:error];
         }
     }
