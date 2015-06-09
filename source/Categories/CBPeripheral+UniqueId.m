fe5265650bb15f2dde605518a2ca5de9cf0d382a
sodastsai <sodas2002@gmail.com>
2015-06-09 17:16:45 +0800

    Fix for iOS 9 API changes
    since currently the bean sdk only supports iOS 7.0+ and OS X 10.9+, old version checks (for iOS 5/6) and compatbility workarounds should be removed.
    Also, APIs like `-[CBPeripheral UUID]` has been removed from iOS 9.

--- source/Categories/CBPeripheral+UniqueId.m
+++ source/Categories/CBPeripheral+UniqueId.m
@@ -13,39 +13,7 @@
 
 - (NSUUID *)uniqueID
 {
-    
-    NSUUID* uuid;
-    
-#if TARGET_OS_IPHONE
-    NSArray *vComp = [[UIDevice currentDevice].systemVersion componentsSeparatedByString:@"."];
-    if ([[vComp objectAtIndex:0] intValue] >= 7) {
-        if(self.identifier) uuid = self.identifier;
-    } else if ([[vComp objectAtIndex:0] intValue] == 6  || [[vComp objectAtIndex:0] intValue] == 5) {
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wdeprecated-declarations"
-        if(self.UUID) uuid = [[NSUUID alloc] initWithUUIDBytes:[[[CBUUID UUIDWithCFUUID:self.UUID] data] bytes]];
-#pragma clang diagnostic pop
-    }
-#else
-    
-#ifdef NSAppKitVersionNumber10_9
-    if (floor(NSAppKitVersionNumber) >= NSAppKitVersionNumber10_9) {
-        if(self.identifier) uuid = self.identifier;
-    }else
-#else
-    {
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wdeprecated-declarations"
-        if(self.UUID) uuid = [[NSUUID alloc] initWithUUIDBytes:[[[CBUUID UUIDWithCFUUID:self.UUID] data] bytes]];
-#pragma clang diagnostic pop
-    }
-#endif
-    
-#endif
-    
-    if (!uuid) uuid = [[NSUUID alloc] initWithUUIDString:@"00000000-0000-0000-0000-000000000000"];
-    
-    return uuid;
+    return self.identifier ?: [[self class] blankID];
 }
 
 +(NSUUID *)blankID
