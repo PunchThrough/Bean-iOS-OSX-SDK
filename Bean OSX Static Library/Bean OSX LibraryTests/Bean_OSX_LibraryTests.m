95fbea0d09a92b42896e64f9c298ebeefb84d9da
Karel Kalthoff <kkalthoff@punchthrough.com>
2016-02-22 14:36:29 -0600

    Adding helper methods and added testConnectBean

--- Bean OSX Static Library/Bean OSX LibraryTests/Bean_OSX_LibraryTests.m
+++ Bean OSX Static Library/Bean OSX LibraryTests/Bean_OSX_LibraryTests.m
@@ -11,7 +11,10 @@
 
 @interface Bean_OSX_LibraryTests : XCTestCase <PTDBeanManagerDelegate>
 
+@property (nonatomic, strong) PTDBeanManager *beanManager;
+
 @property (nonatomic, strong) void (^beanDiscovered)(PTDBean *bean);
+@property (nonatomic, strong) void (^beanConnected)(PTDBean *bean);
 
 @end
 
@@ -20,7 +23,10 @@
 - (void)setUp
 {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+
+    // Prepare BeanManager and make sure it's happy with Bluetooth powered on
+    self.beanManager = [[PTDBeanManager alloc] initWithDelegate:self];
+    [self delayForSeconds:1];
 }
 
 - (void)tearDown
@@ -35,17 +41,6 @@
 {
     // given
     NSString *beanName = @"NEO";
-    PTDBeanManager *beanManager = [[PTDBeanManager alloc] initWithDelegate:self];
-    XCTestExpectation *waitedForBTPoweredOn = [self expectationWithDescription:@"Waited for Bluetooth to power on"];
-
-    // Delay for some time (??) so that CBCentralManager connection state becomes PoweredOn
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        [waitedForBTPoweredOn fulfill];
-    });
-    
-    [self waitForExpectationsWithTimeout:10 handler:nil];
-    
-    // given
     __block PTDBean *targetBean;
     NSError *error;
     
@@ -60,7 +55,8 @@
         }
     };
     
-    [beanManager startScanningForBeans_error:&error];
+    // scan
+    [self.beanManager startScanningForBeans_error:&error];
     if (error) {
         XCTFail(@"startScanningForBeans should not fail");
         return;
@@ -72,39 +68,57 @@
     XCTAssertNotNil(targetBean, @"targetBean should not be nil");
 }
 
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        
-        // given
-        __block PTDBean *targetBean;
-        NSError *error;
-
-        // when
-        XCTestExpectation *beanFound = [self expectationWithDescription:@"Target Bean found"];
-        self.beanDiscovered = ^void(PTDBean *bean) {
-            NSLog(@"Found Bean: %@", bean);
-            if ([bean.name isEqualToString:beanName]) {
-                PTDLog(@"Found target Bean: %@", bean);
-                targetBean = bean;
-                [beanFound fulfill];
-            }
-        };
-        
-        [beanManager startScanningForBeans_error:&error];
-        if (error) {
-            XCTFail(@"startScanningForBeans should not fail");
-            return;
+- (void)testConnectBean
+{
+    // given
+    NSString *beanName = @"NEO";
+    __block PTDBean *targetBean;
+    NSError *error;
+    
+    // when
+    
+    XCTestExpectation *beanConnected = [self expectationWithDescription:@"Target Bean connected"];
+    self.beanConnected = ^void(PTDBean *bean) {
+        NSLog(@"Connected Bean: %@", bean);
+        if ([bean.name isEqualToString:beanName]) {
+            NSLog(@"Connected target Bean: %@", bean);
+            [beanConnected fulfill];
         }
-        
-        [self waitForExpectationsWithTimeout:5 handler:^(NSError * _Nullable error) {
-            XCTFail(@"Timeout while waiting to discover target Bean: %@", beanName);
-            return;
-        }];
-        
-        // then
-        XCTAssertNotNil(targetBean, @"targetBean should not be nil");
-    });
-}
+    };
+    
+    self.beanDiscovered = ^void(PTDBean *bean) {
+        NSLog(@"Found Bean: %@", bean);
+        if ([bean.name isEqualToString:beanName]) {
+            NSLog(@"Found target Bean: %@", bean);
+            targetBean = bean;
 
+            
+            // connect
+            NSError *connectError;
+            [self.beanManager connectToBean:targetBean error:&connectError];
+            // connectError always throws a "connection in progress" error, so don't assert that it is not nil
+            // TODO: Isolate, reproduce error, figure out why this happens
+        }
+    };
+    
+    // scan
+    [self.beanManager startScanningForBeans_error:&error];
+    if (error) {
+        XCTFail(@"startScanningForBeans should not fail");
+        return;
+    }
+    
+    // then
+    [self waitForExpectationsWithTimeout:20 handler:nil];
+    XCTAssertTrue(targetBean.state == BeanState_ConnectedAndValidated);
+    
+    // cleanup
+    NSError *disconnectError;
+    [self.beanManager disconnectBean:targetBean error:&disconnectError];
+    XCTAssertNil(disconnectError);
+    
+    [self delayForSeconds:2];
+}
 
 #pragma mark - bean manager delegate
 
@@ -116,6 +130,26 @@
     }
 }
 
-#pragma mark - bean delegates
+- (void)BeanManager:(PTDBeanManager *)beanManager didConnectToBean:(PTDBean *)bean error:(NSError *)error
+{
+    NSLog(@"Connected Bean: %@", bean);
+    if (self.beanConnected) {
+        self.beanConnected(bean);
+    }
+}
+
+#pragma mark - test helpers
+
+- (void)delayForSeconds:(NSInteger)seconds
+{
+    XCTestExpectation *waitedForXSeconds = [self expectationWithDescription:@"Waited for some specific time"];
+    
+    // Delay for some time (??) so that CBCentralManager connection state becomes PoweredOn
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(seconds * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [waitedForXSeconds fulfill];
+    });
+    
+    [self waitForExpectationsWithTimeout:seconds + 1 handler:nil];
+}
 
 @end
