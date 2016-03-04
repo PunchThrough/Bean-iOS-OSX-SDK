765f5b19b6043aaecc0cfec9a2ff554de30a1830
Matthew Lewis <matt@mplewis.com>
2016-03-04 15:17:27 -0600

    helper: firmwareUpdateRequiredForBean docs and test skeleton

---
+++ Bean OSX Static Library/Bean OSX LibraryTests/TestBeanHelpers.m
@@ -0,0 +1,17 @@
+#import <XCTest/XCTest.h>
+
+@interface TestBeanHelpers : XCTestCase
+
+@end
+
+@implementation TestBeanHelpers
+
+/**
+ *  Ensure that firmwareUpdateRequiredForBean returns proper values for different firmwares and Beans.
+ */
+- (void)testfirmwareUpdateRequiredForBean
+{
+    // TBD
+}
+
+@end
--- source/Helper/BEAN_Helper.h
+++ source/Helper/BEAN_Helper.h
@@ -36,5 +36,14 @@
 
 + (NSData*)dummyData:(NSInteger)length;
 
-+ (BOOL)firmwareUpdateRequiredForBean:(PTDBean *)bean withBakedFirmware:(NSString *)bakedVersion;
+/**
+ *  Check if a Bean is running out-of-date firmware and should be updated. This method takes into account the firmware
+ *  that is available in the client. If Bean has newer firmware than the client, this returns NO.
+ *  @param bean The Bean to be inspected for firmware state
+ *  @param version The firmware version available in the client. Should be a simple 12-digit datestamp that can be
+ *      parsed as an integer, i.e. "201501110000"
+ *  @return YES if Bean's firmware is out of date, NO if Bean has equivalent or newer firmware
+ */
++ (BOOL)firmwareUpdateRequiredForBean:(PTDBean *)bean availableFirmware:(NSString *)version;
+
 @end
