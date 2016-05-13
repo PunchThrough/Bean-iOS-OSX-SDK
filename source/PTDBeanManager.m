a1810ab8ee793a0de5824b75bc78203483f2b515
Karel Kalthoff <kkalthoff@punchthrough.com>
2016-05-13 13:59:34 -0500

    improvement: no longer delaying reconnect for iPhone

--- source/PTDBeanManager.m
+++ source/PTDBeanManager.m
@@ -344,33 +344,10 @@ NSString * const PTDBeanManagerConnectionOptionProfilesRequiredToConnect    = @"
     if(!bean)
         return; //This may not be the best way to handle this case
 
-#if TARGET_OS_IPHONE
-
-    if (bean.updateInProgress) {
-        /*
-         If we reconnect to Bean too quickly, we will reuse the old GATT table. During an update, we expect the GATT
-         table to change, and using the cached GATT table will cause unexpected behavior. By waiting until Bean has
-         reset fully before attempting to connect again, we ensure that iOS doesn't use the cached table.
-         */
-        PTDLog(@"Auto-reconnecting to %@ in 2 seconds", bean);
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 2 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
-            PTDLog(@"Auto-reconnecting to %@", bean);
-            [self connectToBean:bean error:nil];
-        });
-
-    } else if (bean.autoReconnect) {
-        PTDLog(@"Auto-reconnecting to %@", bean);
-        [self connectToBean:bean error:nil];
-    }
-
-#else
-
     if (bean.updateInProgress || bean.autoReconnect) {
         PTDLog(@"Auto-reconnecting to %@", bean);
         [self connectToBean:bean error:nil];
     }
-
-#endif
 
     //Alert the delegate of the disconnect
     [self __notifyDelegateOfDisconnectedBean:bean error:error];
